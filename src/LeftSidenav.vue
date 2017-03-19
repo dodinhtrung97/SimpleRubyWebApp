be2e5b98a2566003479d8b1df01eff624f9405a7
Sky <dodinhtrung97@gmail.com>
2017-03-20 03:45:06 +0700

    Added Navigation Bar buttons

---
+++ src/LeftSidenav.vue
@@ -0,0 +1,59 @@
+x`<template lang="html">
+  <div class="left-sidenav">
+    <md-toolbar>
+      <md-button class="md-icon-button" @click.native="toggleLeftSidenav">
+        <md-icon>menu</md-icon>
+      </md-button>
+      <h2 class="md-title">Homework 2</h2>
+    </md-toolbar>
+    <md-sidenav class="md-left" ref="leftSidenav">
+      <md-toolbar class="md-large">
+        <div class="md-toolbar-container">
+          <h3 class="md-title">Navigate</h3>
+        </div>
+      </md-toolbar>
+      <md-content flex role="navigation">
+             <md-list>
+                <md-list-item>
+                    <md-button href="#/posts">Post Index</md-button>
+                </md-list-item>
+                <md-list-item>
+                    <md-button href="#/posts/new">New Post</md-button>
+                </md-list-item>
+                <md-list-item>
+                    <md-button @click.native="logout">Logout</md-button>
+                </md-list-item>
+            </md-list>
+        </md-content>
+    </md-sidenav>
+  </div>
+</template>
+
+<script>
+import UsersApi from './api/users.js'
+import router from './router'
+
+export default {
+  name: 'sign-out',
+  data () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    logout () {
+      console.log('methods.logout')
+      UsersApi.logout(function (_response) {
+        router.push({ name: 'Posts.index' })
+      })
+    },
+    toggleLeftSidenav () {
+      this.$refs.leftSidenav.toggle()
+    }
+  }
+}
+</script>
+
+<style lang="css">
+</style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-// ---------------------
+import store from './store'
+import UserApi from '@/api/users'
+
 import VueMaterial from 'vue-material'
 import 'vue-material/dist/vue-material.css'
 
@@ -9,14 +11,19 @@ Vue.use(VueMaterial)
 
 import Axios from 'axios'
 Vue.$http = Axios
-// ---------------------
+
+Axios.defaults.baseURL = 'https://iccs340-vue-api-auth.herokuapp.com/'
+Axios.defaults.headers.common.Accept = 'application/json'
+Axios.defaults.withCredentials = true
 
 Vue.config.productionTip = false
 
+UserApi.checkLoggedIn()
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
   router,
+  store,
   template: '<App/>',
   components: { App }
 })
